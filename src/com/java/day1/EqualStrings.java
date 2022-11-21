aa945fe4d12ae0fa640408fb0577fa3327ed2cd5
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-11-21 17:02:38 +0530

    Added:EqualString Program

---
+++ src/com/java/day1/EqualStrings.java
@@ -0,0 +1,22 @@
+package com.java.day1;
+
+import java.util.Scanner;
+
+public class EqualStrings {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the first String");
+		String s1 = sc.next();
+		
+		System.out.println("Enter the Second String");
+		String s2 = sc.next();
+		
+		if(s1.equals(s2))
+			System.out.println("Both Strings are equal");
+		else
+			System.out.println("Both Strings are not equal");
+
+	}
+
+}
