774d17997830a400196122814e5017f545b0a956
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-11-23 09:16:17 +0530

    Added:logic to display the welcome message

--- src/com/java/day1/DisplayAMessage.java
+++ src/com/java/day1/DisplayAMessage.java
@@ -1,7 +1,7 @@
 package com.java.day1;
 
 public class DisplayAMessage {
-
+	//driver class
 	public static void main(String[] args) {
 		System.out.println("Welcome to the java programing language");
 
