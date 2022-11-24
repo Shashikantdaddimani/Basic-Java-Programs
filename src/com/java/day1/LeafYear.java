8f4b0a9e42799a39929bfc54ab1cec5e14712d49
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-11-24 13:02:24 +0530

    Added:logic to find a year is leaf year

--- src/com/java/day1/LeafYear.java
+++ src/com/java/day1/LeafYear.java
@@ -1,15 +1,18 @@
 package com.java.day1;
 
+//imports the scanner class of the java.utill package
 import java.util.Scanner;
 
+//class definition
 public class LeafYear {
-
+	//main method
 	public static void main(String[] args) {
 		//Taking user input
-			 Scanner sc = new Scanner(System.in);
+			 Scanner input = new Scanner(System.in);
 			 System.out.println("Enter a year to check");
-			 int year = sc.nextInt();
+			 int year = input.nextInt();
 			 
+			//Logic to check the a year is leaf year
 			if (year % 400==0) {
 				System.out.println(year + " is a leaf year. ");
 			}else if (year % 100==0) {
